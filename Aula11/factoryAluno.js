50cd1922a7dea1c9b8b5be1d5e8b1d6a767620cf
Fabio Fiorita <fabiolfp@gmail.com>
2022-11-02 15:46:09 -0300

    Creating Aula12

--- Aula11/factoryAluno.js
+++ Aula11/factoryAluno.js
@@ -1,13 +1,13 @@
 const aluno = (nome, idade, matricula) => ({
-    nome,
-    idade,
-    matricula,
-    getNome: () => nome,
-    getIdade: () => idade,
-    getMatricula: () => matricula,
-})
+  nome,
+  idade,
+  matricula,
+  getNome: () => nome,
+  getIdade: () => idade,
+  getMatricula: () => matricula,
+});
 
-const aluno1 = aluno('João', 20, 1234);
+const aluno1 = aluno("João", 20, 1234);
 console.log(aluno1.getNome());
 console.log(aluno1.getIdade());
 console.log(aluno1.getMatricula());
--- Aula8/src/infrastructure/rest_server.js
+++ Aula8/src/infrastructure/rest_server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-require('../port/car_api')(app);
+require("../port/car_api")(app);
 
 module.exports = app;
